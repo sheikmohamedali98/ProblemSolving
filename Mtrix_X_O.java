33fccea35f77e44f12ea6397251d07ffbd40c295
sheikmohamedali98 <103345551+sheikmohamedali98@users.noreply.github.com>
2022-07-17 17:39:22 +0530

    Matrix X_O

--- Mtrix_X_O.java
+++ Mtrix_X_O.java
@@ -1,7 +1,35 @@
 package Practis;
 
 import java.util.Scanner;
+/*
 
+Input:
+N= 3, M=3
+ 
+Output:
+X X X
+X 0 X
+X X X
+Input:
+  N=4 M=5 
+Output:
+X X  X X
+X  0  0 X
+X  0  0 X
+X  0  0 X
+X X X X
+ 
+Input:
+N=6 M=7
+
+X X X X X X
+X 0  0  0  0 X
+X 0  X  X 0 X
+X 0  X  X 0 X
+X 0  X  X 0 X
+X 0  0  0  0 X
+X X X X X X
+*/
 public class Mtrix_X_O {
 
 	private static Scanner scan = new Scanner(System.in);
