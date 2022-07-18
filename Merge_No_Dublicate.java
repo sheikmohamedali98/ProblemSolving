b39713377129ab2a29ac8b7f647877b212a29570
sheikmohamedali98 <103345551+sheikmohamedali98@users.noreply.github.com>
2022-07-18 11:30:34 +0530

    Merge_Number_No_Dublicate

--- Merge_No_Dublicate.java
+++ Merge_No_Dublicate.java
@@ -1,5 +1,13 @@
 package Practis;
+/*
+Given two sorted arrays, merge them such that the elements are not repeated
 
+Eg 1: Input:
+        Array 1: 2,4,5,6,7,9,10,13
+        Array 2: 2,3,4,5,6,7,8,9,11,15
+       Output:
+       Merged array: 2,3,4,5,6,7,8,9,10,11,13,15 
+*/
 public class Merge_No_Dublicate {
 
 	public static void main(String[] args) {
